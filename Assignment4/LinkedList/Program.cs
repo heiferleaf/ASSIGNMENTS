4a0d72ea798d43229c7fb7a3922553cc1aa35acf
Heife <2044384845@qq.com>
2025-03-13 16:56:05 +0800

    第四次作业

---
+++ Assignment4/LinkedList/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace HW4.LinkedList
+{
+    // 链表节点
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+
+        public Node(T data) { 
+            Data = data;
+            Next = null;
+        }
+
+        public Node() {
+            Next = null;
+        }
+    }
+
+    // 链表
+    public class GenericList<T>
+    {
+        private Node<T> head, tail;
+
+        public GenericList()
+        {
+            tail = null;
+            head = new Node<T>();
+        }
+
+        // 创建一个链表
+        public void insertNode(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            if(head.Next == null)
+            {
+                head.Next = node;
+                tail = node;
+            }
+            else
+            {
+                tail.Next = node;
+                tail = node;
+            }
+        }
+
+        public void createList(T[] datas)
+        {
+            for(int i = 0; i < datas.Length; i++)
+            {
+                insertNode(datas[i]);
+            }
+        }
+
+        public void ForEach(Action<Node<T>> action)
+        {
+            action(head.Next);
+        }
+    }
+
+    public class Test
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> list = new GenericList<int>();
+            list.createList([1, 2, 3, 4, 5]);
+            // 输出所有数
+            list.ForEach(p =>
+            {
+                Console.WriteLine("output all datas");
+                while (p != null)
+                {
+                    Console.Write(p.Data + " ");
+                    p = p.Next;
+                }
+                Console.WriteLine();
+            });
+            // 求最大值
+            list.ForEach(p =>
+            {
+                Console.WriteLine("get max");
+                int max = int.MinValue;
+                while (p != null)
+                {
+                    if (max < p.Data)
+                        max = p.Data;
+                    p = p.Next;
+                }
+                Console.WriteLine(max);
+            });
+            // 求最小值
+            list.ForEach(p =>
+            {
+                Console.WriteLine("get min");
+                int min = int.MaxValue;
+                while (p != null)
+                {
+                    if (min > p.Data)
+                        min = p.Data;
+                    p = p.Next;
+                }
+                Console.WriteLine(min);
+            });
+            // 求和
+            list.ForEach(p =>
+            {
+                Console.WriteLine("get sum");
+                int sum = 0;
+                while (p != null)
+                {
+                    sum += p.Data;
+                    p = p.Next;
+                }
+                Console.WriteLine(sum);
+            });
+        }
+    }
+}
---
+++ Assignment4/alarmClock/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+//使用事件机制，模拟实现一个闹钟功能。
+//闹钟可以有嘀嗒（Tick）事件和响铃（Alarm）两个事件。在闹钟走时时或者响铃时，在控制台显示提示信息
+
+namespace HW4.alarmClock
+{
+    class ClockEventArgs : EventArgs
+    {
+        internal int Second { get; set; }
+        internal int Minute { get; set; }
+        internal int Clock { get; set; }
+
+        public ClockEventArgs(int Second, int Minute, int Clock)
+        {
+            this.Second = Second;
+            this.Minute = Minute;
+            this.Clock = Clock;
+        }
+
+    }
+
+    class ClockPublish
+    {
+        public event EventHandler<ClockEventArgs> clockEvent;
+
+        public void sendMessage(int Second, int Minute, int Clock)
+        {
+            clockEvent?.Invoke(this, new ClockEventArgs(Second, Minute, Clock));
+        }
+    }
+
+    public class Test
+    {
+        static void Main(string[] args)
+        {
+            ClockPublish cp = new ClockPublish();
+
+            int setSecond = 0, setMinute = 0, setClock = 0;
+            try
+            {
+                setSecond = int.Parse(Console.ReadLine());
+                setMinute = int.Parse(Console.ReadLine());
+                setClock = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("输入无效，请输入一个有效的整数。");
+            }
+
+            // 增加时钟滴答功能
+            cp.clockEvent += (sender, clock) =>
+                Console.WriteLine($"滴答， 当前是北京时间 {clock.Clock} 时 {clock.Minute} 分 {clock.Second} 秒");
+            // 响铃功能
+            cp.clockEvent += (sender, clock) =>
+                Console.WriteLine((clock.Clock == setClock && clock.Minute == setMinute && clock.Second == setSecond) ? "闹钟响起" : " ");
+
+            for (int c = 0; c < 24; c++)
+                for (int m = 0; m < 60; m++)
+                    for (int s = 0; s < 60; s++)
+                    {
+                        cp.sendMessage(s, m, c);
+                    }
+        }
+    }
+}
